07ae88f178b470d74729373a55e246022d1c5021
kawate <hiroyuki.kawate@alfredcore.com>
2012-11-06 10:34:59 +0900

    新たなオススメ機能

--- git-tutorial/GTRootViewController.m
+++ git-tutorial/GTRootViewController.m
@@ -22,6 +22,12 @@
   [self setHelloButton];
 
   self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+  [self setLabelToTestView];
+  UITapGestureRecognizer *tapGesture = [[UITapGestureRecognizer alloc]
+                                        initWithTarget:self
+                                        action:@selector(tapTestView:)];
+  [_testView addGestureRecognizer:tapGesture];
 }
 
 -(void)setHelloButton{
@@ -35,5 +41,34 @@
 -(void)tapHello:(UIButton *)button{
   UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Hello YAMASHITA" message:nil delegate:nil cancelButtonTitle:@"了解" otherButtonTitles:nil];
   [alert show];
+}
+
+-(void)setLabelToTestView{
+  UILabel *testViewLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0,
+                                                                     _testView.frame.size.width,
+                                                                     _testView.frame.size.height)];
+  testViewLabel.backgroundColor = [UIColor clearColor];
+  testViewLabel.textAlignment = NSTextAlignmentCenter;
+  testViewLabel.textColor = [UIColor cyanColor];
+  testViewLabel.minimumScaleFactor = 1.0f;
+  testViewLabel.adjustsFontSizeToFitWidth = YES;
+  testViewLabel.adjustsLetterSpacingToFitWidth = YES;
+  testViewLabel.font = [UIFont boldSystemFontOfSize:40];
+  testViewLabel.text = @"Tap";
+  [_testView addSubview:testViewLabel];
+}
+
+-(void)tapTestView:(id)sender{
+  [UIView animateWithDuration:3.0 animations:^{
+    CGAffineTransform scale = CGAffineTransformMakeScale(1.5f, 1.5f);
+    CGAffineTransform trans = CGAffineTransformMakeTranslation(88, 88);
+    CGAffineTransform rotate = CGAffineTransformMakeRotation((M_PI/180)*180);
+    _testView.transform = CGAffineTransformConcat(scale, CGAffineTransformConcat(trans, rotate));
+  } completion:^(BOOL finished) {
+    [UIView animateWithDuration:1.0f animations:^{
+      _testView.transform = CGAffineTransformIdentity;
+    } completion:^(BOOL finished) {
+    }];
+  }];
 }
 @end
