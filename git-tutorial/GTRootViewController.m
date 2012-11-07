2a95b8507d26e835c671a43cd6ae8ae347f51991
Hiroshi Awashima <awa@e-mail.jp>
2012-11-07 11:49:01 +0900

    tb001 add label

--- git-tutorial/GTRootViewController.m
+++ git-tutorial/GTRootViewController.m
@@ -12,6 +12,10 @@
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.view.backgroundColor = [UIColor orangeColor];
+  
+  UILabel* _label = [[UILabel alloc]initWithFrame:CGRectMake(100,100, 300,300)];
+  _label.text = @"TB001";
+  [self.view addSubview:_label];
 
   self.navigationItem.rightBarButtonItem = self.editButtonItem;
 }
