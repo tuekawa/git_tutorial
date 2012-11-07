947a98485b42b228707da1d95af1b8b712acff29
Hiroshi Awashima <awa@e-mail.jp>
2012-11-07 17:46:17 +0900

    画面２への遷移ボタンを追加した

--- git-tutorial/GTRootViewController.m
+++ git-tutorial/GTRootViewController.m
@@ -1,4 +1,5 @@
 #import "GTRootViewController.h"
+#import "GTScreen2ViewController.h"
 
 @implementation GTRootViewController
 
@@ -13,11 +14,24 @@
   [super viewDidLoad];
   self.view.backgroundColor = [UIColor orangeColor];
   
-  UILabel* label = [[UILabel alloc]initWithFrame:CGRectMake(100,100, 300,300)];
+  UILabel* label = [[UILabel alloc]initWithFrame:CGRectMake(100,100, 30,100)];
   label.text = @"TB001";
   [self.view addSubview:label];
 
   self.navigationItem.rightBarButtonItem = self.editButtonItem;
+
+  // 画面２への遷移ボタン
+  UIButton *button2 = [UIButton buttonWithType:UIButtonTypeRoundedRect];
+  button2.frame = CGRectMake((320-220)/2, 320, 220, 40);
+  [button2 setTitle:@"画面２へ遷移" forState:UIControlStateNormal];
+  [button2 addTarget:self action:@selector(pushButton2:) forControlEvents:UIControlEventTouchUpInside];
+  [self.view addSubview:button2];
+  
+}
+
+- (void)pushButton2:(UIButton *)button {
+  GTScreen2ViewController *screen2ViewController = [[GTScreen2ViewController alloc] init];
+  [self.navigationController pushViewController:screen2ViewController animated:YES];
 }
 
 @end
---
+++ git-tutorial/GTScreen2ViewController.h
@@ -0,0 +1,13 @@
+//
+//  GTScreen2ViewController.h
+//  git-tutorial
+//
+//  Created by 淡島 弘吏 on 2012/11/07.
+//  Copyright (c) 2012年 Takeshi Shoji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface GTScreen2ViewController : UIViewController
+
+@end
